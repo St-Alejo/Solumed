3ed0055f90521e1fda75a4142d85c4d7b03f354b
Steven <steven.alejo1508@gmail.com>
2026-03-02 11:42:16 -0500

    change config

--- solumed_back/backend/app/core/config.py
+++ solumed_back/backend/app/core/config.py
@@ -46,6 +46,7 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:3001",
+        "https://solumed.vercel.app",
     ]
 
     @property
